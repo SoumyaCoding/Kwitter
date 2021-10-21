6fb08ab76f64a3d1761834c8e02b16b53427411b
SoumyaCoding <86145656+SoumyaCoding@users.noreply.github.com>
2021-10-21 16:42:18 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -2,4 +2,28 @@ function adduser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
+
+}
+
+function addRoom(){
+ room_name = document.getElementById("room_name").value;  
+    firebase.database().ref("/").chid(room_name).update({
+        pursose :"adding room name"
+});
+   localStorage.setItem("room_name", room_name_name);
+    window.location = "kwitter_room.html";
+ 
+}
+function getData(){firebase.database().ref("/").on("value" , (snapshot) {document.getElementById("output").innerHTML
+Room_names = childKey;
+row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>"
+document.getElementById("output").innerHTML += row
+};);}
+getData()
+
+function redirectToRoomName()
+{
+    localStorage.setItem("room_name",name);
+    window.location = "kwitter_room.html";
+
 }
